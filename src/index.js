65e32701471aab86f35692a11eed73bbfabea6c9
KaitoTakashima <61674582+KaitoTakashima@users.noreply.github.com>
2021-06-19 09:47:36 +0900

    variable_test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-console.log("テスト");
-//テスト
+// console.log("テスト");
+// //テスト
+
+// const test = "kai";
+// test = "kai2"; //再代入エラー
+// console.log(test);
+
+// let test = "kai";
+// test = "kai2"; //再代入できる
+// let test = "kai3"; //再宣言エラー
+
+// var test = "kai";
+// test = "kai2"; //再代入できる
+// var test = "kai3"; //再宣言できる
